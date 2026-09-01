38217243d14783eb7c0d8f49b40a29d6c1100589
raynor-koh <64368303+raynor-koh@users.noreply.github.com>
2026-09-01 18:52:46 +0800

    feat: add echo user input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -30,8 +30,14 @@ public class Main extends Application {
         userInput = new TextField();
         sendButton = new Button("Send");
 
-        DialogBox dialogBox = new DialogBox("Hello!", userImage);
-        dialogContainer.getChildren().addAll(dialogBox);
+        // Handling user input
+
+        sendButton.setOnMouseClicked((event) -> {
+            handleUserInput();
+        });
+        userInput.setOnAction((event) -> {
+            handleUserInput();
+        });
 
         AnchorPane mainLayout = new AnchorPane();
         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
@@ -72,5 +78,14 @@ public class Main extends Application {
         AnchorPane.setBottomAnchor(userInput, 1.0);
 
         // More code to be added here later
+    }
+
+    /**
+     * Creates a dialog box containing user input, and appends it to
+     * the dialog container. Clears the user input after processing.
+     */
+    private void handleUserInput() {
+        dialogContainer.getChildren().addAll(new DialogBox(userInput.getText(), userImage));
+        userInput.clear();
     }
 }
